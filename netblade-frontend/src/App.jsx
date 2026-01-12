c33d6b44f85cb4b35aab2f962ac51390a581c1d7
Anunay Raj <anunay.r24@gmail.com>
2026-01-12 22:30:51 +0530

    inital Netblade frontend  setup = vite+react+router

---
+++ netblade-frontend/src/App.jsx
@@ -0,0 +1,6 @@
+import AppRoutes from "./AppRoutes";
+
+export default function App() {
+  return <AppRoutes />;
+}
+
---
+++ netblade-frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+    </nav>
+  );
+}
